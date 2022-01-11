dff32618a297d3119f0f9a8ec54807e1dbd695b7
TropicolCoder <jacobsbusayo@gmail.com>
2022-01-11 10:54:38 +0100

    fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-import HomePageTemplate from 'components/templates/HomePage'
+import HomePageTemplate from "components/templates/HomePage";
 
 const HomePage = (): JSX.Element => {
-  return (
-    <>
-      <Head>
-        <title>Audiophile Shop</title>
-      </Head>
-      <HomePageTemplate />
-    </>
-  )
-}
+    return (
+        <>
+            <Head>
+                <title>Audiophile Shop</title>
+            </Head>
+            <HomePageTemplate />
+        </>
+    );
+};
 
-export default HomePage
+export default HomePage;
